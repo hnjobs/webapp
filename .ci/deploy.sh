5bbb9e17992bee335f390740df88ba736be7936d
Emil Burzo <contact@emilburzo.com>
2022-09-27 21:29:19 +0300

    deploy to kubernetes

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-set -eu
-
-mvn package
-
-ssh root@hnjobs '/etc/init.d/jetty8 stop'
-
-scp target/HnJobs-1.0-SNAPSHOT.war root@hnjobs:/var/lib/jetty8/webapps/root.war
-
-ssh root@hnjobs '/etc/init.d/jetty8 start'
-
+kubectl apply -f .ci/hnjobs-webapp.yaml
