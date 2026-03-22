6586cdbaeae319ae1a2fed85d00b98462b5796b4
Emil Burzo <contact@emilburzo.com>
2026-03-22 21:56:20 +0200

    upgrade elasticsearch

--- src/main/java/com/emilburzo/hnjobs/server/manager/SearchManager.java
+++ src/main/java/com/emilburzo/hnjobs/server/manager/SearchManager.java
@@ -4,21 +4,28 @@ import com.emilburzo.hnjobs.shared.rpc.JobRPC;
 import com.emilburzo.hnjobs.shared.rpc.SearchLogRPC;
 import com.emilburzo.hnjobs.shared.rpc.SearchResultRPC;
 import com.google.gson.Gson;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.action.search.SearchType;
-import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.text.Text;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.xcontent.XContentType;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.SimpleQueryStringFlag;
 import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.elasticsearch.search.sort.SortOrder;
+import org.elasticsearch.search.suggest.SuggestBuilder;
 import org.elasticsearch.search.suggest.phrase.PhraseSuggestionBuilder;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.io.IOException;
 import java.util.Date;
 import java.util.logging.Logger;
 
@@ -28,8 +35,7 @@ public class SearchManager {
     private static final String ELASTICSEARCH_PORT = "ELASTICSEARCH_PORT";
 
     private static final String INDEX_HNJOBS = "hnjobs";
-    private static final String TYPE_JOB = "job";
-    private static final String TYPE_SEARCH = "search";
+    private static final String INDEX_SEARCH_LOG = "hnjobs_search_log";
 
     private static final String FIELD_BODY = "body";
     private static final String FIELD_TIMESTAMP = "timestamp";
@@ -42,7 +48,7 @@ public class SearchManager {
 
     private Logger log = Logger.getLogger(getClass().getSimpleName());
 
-    private TransportClient client;
+    private RestHighLevelClient client;
 
     public SearchResultRPC search(String query) {
         try {
@@ -57,48 +63,58 @@ public class SearchManager {
 
             // return results to the client
             return results;
-        } catch (UnknownHostException e) {
+        } catch (IOException e) {
             // todo
             e.printStackTrace();
         } finally {
             // cleanup
-            client.close();
+            try {
+                client.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
         return null;
     }
 
-    private void log(String query, int results, long durationMs) {
-        SearchLogRPC log = new SearchLogRPC(query, results, durationMs);
+    private void log(String query, int results, long durationMs) throws IOException {
+        SearchLogRPC logEntry = new SearchLogRPC(query, results, durationMs);
 
-        IndexResponse response = client.prepareIndex(INDEX_HNJOBS, TYPE_SEARCH)
-                .setSource(new Gson().toJson(log))
-                .get();
+        IndexRequest request = new IndexRequest(INDEX_SEARCH_LOG)
+                .source(new Gson().toJson(logEntry), XContentType.JSON);
+        IndexResponse response = client.index(request, RequestOptions.DEFAULT);
     }
 
-    private SearchResultRPC getResults(String query) {
+    private SearchResultRPC getResults(String query) throws IOException {
         long start = new Date().getTime();
 
         SearchResultRPC rpc = new SearchResultRPC();
 
-        SearchResponse response = client.prepareSearch(INDEX_HNJOBS)
-                .setTypes(TYPE_JOB)
-                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-                .setQuery(QueryBuilders.simpleQueryStringQuery(query)
-                                .field(FIELD_BODY_HTML)
-                                .flags(SimpleQueryStringFlag.ALL)
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder()
+                .query(QueryBuilders.simpleQueryStringQuery(query)
+                        .field(FIELD_BODY_HTML)
+                        .flags(SimpleQueryStringFlag.ALL)
                 )
-                .addSort(FIELD_SCORE, SortOrder.DESC)
-                .addSort(FIELD_TIMESTAMP, SortOrder.DESC)
-                .addSuggestion(new PhraseSuggestionBuilder("default")
-                                .field(FIELD_BODY_HTML)
+                .sort(FIELD_SCORE, SortOrder.DESC)
+                .sort(FIELD_TIMESTAMP, SortOrder.DESC)
+                .suggest(new SuggestBuilder()
+                        .addSuggestion("default", new PhraseSuggestionBuilder(FIELD_BODY_HTML)
                                 .text(query)
                                 .size(MAX_SUGGESTIONS)
+                        )
                 )
-                .addHighlightedField(FIELD_BODY_HTML, 0, 0)
-                .setFrom(0).setSize(MAX_SEARCH_RESULTS).setExplain(false)
-                .execute()
-                .actionGet();
+                .highlighter(new HighlightBuilder()
+                        .field(FIELD_BODY_HTML, 0, 0)
+                )
+                .from(0)
+                .size(MAX_SEARCH_RESULTS)
+                .explain(false);
+
+        SearchRequest searchRequest = new SearchRequest(INDEX_HNJOBS)
+                .source(sourceBuilder);
+
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
 
         // suggested search
         try {
@@ -120,8 +136,9 @@ public class SearchManager {
         return rpc;
     }
 
-    private JobRPC getResult(SearchHit hit) {
-        GetResponse response = client.prepareGet(INDEX_HNJOBS, TYPE_JOB, hit.getId()).get();
+    private JobRPC getResult(SearchHit hit) throws IOException {
+        GetRequest getRequest = new GetRequest(INDEX_HNJOBS, hit.getId());
+        GetResponse response = client.get(getRequest, RequestOptions.DEFAULT);
 
         JobRPC job = new JobRPC();
 
@@ -141,12 +158,12 @@ public class SearchManager {
      * @return
      */
     private String getBody(SearchHit hit) {
-        if (hit == null || hit.getSource() == null) {
+        if (hit == null || hit.getSourceAsMap() == null) {
             return "";
         }
 
         if (hit.getHighlightFields() == null || hit.getHighlightFields().get(FIELD_BODY_HTML) == null) {
-            return hit.getSource().get(FIELD_BODY_HTML).toString();
+            return hit.getSourceAsMap().get(FIELD_BODY_HTML).toString();
         }
 
         Text[] fragments = hit.getHighlightFields().get(FIELD_BODY_HTML).getFragments();
@@ -159,10 +176,12 @@ public class SearchManager {
         return body;
     }
 
-    private void initEs() throws UnknownHostException {
-        client = TransportClient.builder().build().addTransportAddress(new InetSocketTransportAddress(
-                InetAddress.getByName(System.getenv().getOrDefault(ELASTICSEARCH_HOST, "hnjobs")),
-                Integer.parseInt(System.getenv().getOrDefault(ELASTICSEARCH_PORT, "9300")))
+    private void initEs() {
+        String host = System.getenv().getOrDefault(ELASTICSEARCH_HOST, "localhost");
+        int port = Integer.parseInt(System.getenv().getOrDefault(ELASTICSEARCH_PORT, "9200"));
+
+        client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost(host, port, "http"))
         );
     }
 }
